d730915ed73b999792dbdc2fd91cbf2fee46c21e
giantconiku <giantconik@gmail.comu>
2024-05-05 18:08:14 +0200

    Created Spring Boot Application

---
+++ src/test/java/com/giant/springbootrestapipostgresql/SpringBootRestApiPostgreSqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.giant.springbootrestapipostgresql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestApiPostgreSqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
